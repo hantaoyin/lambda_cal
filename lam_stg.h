cdf9f7cd9f0186c51ce4903e046516ae5c15c363
Hantao Yin <yinnht@gmail.com>
2013-05-06 21:52:26 -0400

    Added code for closure update in lam_stg.h.

--- lam_stg.h
+++ lam_stg.h
@@ -10,7 +10,8 @@
 
 #define ARG_SIZE_MAX 5000
 #define RET_SIZE_MAX 50000
-#define HEAP_SIZE_MAX 2000000
+#define UPD_SIZE_MAX 5000
+#define HEAP_SIZE_MAX 1500000
 
 //#define NDEBUG
 #include<assert.h>
@@ -24,6 +25,14 @@ typedef struct closure {
     size_t fv_cnt;
 } closure;
 
+typedef struct update_frame {
+    closure **arg_fp;
+    closure **ret_fp;
+    size_t arg_size;
+    size_t ret_size;
+    closure *updatable;
+} update_frame;
+
 // FIXME: flexible array member in struct: a C99 feature!
 typedef struct symbol {
     closure c;
@@ -31,13 +40,17 @@ typedef struct symbol {
 } symbol;
 
 closure *cur_closure;
-closure **arg_stack;
-closure **ret_stack;
+update_frame *upd_base;
+closure **arg_base;
+closure **ret_base;
+closure **arg_fp;
+closure **ret_fp;
 char *heap;
 char *old_heap;
 
 size_t arg_size;
 size_t ret_size;
+size_t upd_size;
 size_t heap_size;
 
 ////////////////////////////////////////////////////////////////////////
@@ -57,16 +70,18 @@ void initialize(void)
     clock_gettime(CLOCK_REALTIME, &main_start);
 #endif
     
-    arg_stack = (closure **)malloc(sizeof(closure *) * ARG_SIZE_MAX);
-    ret_stack = (closure **)malloc(sizeof(closure *) * RET_SIZE_MAX);
+    arg_base = arg_fp = (closure **)malloc(sizeof(closure *) * ARG_SIZE_MAX);
+    ret_base = ret_fp = (closure **)malloc(sizeof(closure *) * RET_SIZE_MAX);
+    upd_base = (update_frame *)malloc(sizeof(update_frame) * UPD_SIZE_MAX);
     heap      = (char *)malloc(HEAP_SIZE_MAX);
     old_heap  = (char *)malloc(HEAP_SIZE_MAX);
 }
 
 void finalize(void)
 {
-    free(arg_stack);
-    free(ret_stack);
+    free(arg_fp);
+    free(ret_fp);
+    free(upd_base);
     free(heap);
     free(old_heap);
 
@@ -141,12 +156,16 @@ closure *move_closure(closure *p)
     return pnew;
 }
 
+closure *run_indirection(void);
 closure *try_move(closure *p)
 {
     if(p->code == NULL) {
         return (closure *)(heap + p->fv_cnt);
     } else if(p->code == run_ubsym) {
         return p;
+    } else if(p->code == run_indirection) {
+        closure **fv_ptr = (closure **)(p + 1);
+        return try_move(fv_ptr[0]);
     } else {
         return move_closure(p);
     }
@@ -173,11 +192,19 @@ void gc(void)
     cur_closure = try_move(cur_closure);
 
     size_t i;
-    for(i = 0; i < arg_size; ++i) {
-        arg_stack[i] = try_move(arg_stack[i]);
+    size_t arg_total = arg_size + (arg_fp - arg_base);
+    for(i = 0; i < arg_total; ++i) {
+        arg_base[i] = try_move(arg_base[i]);
     }
-    for(i = 0; i < ret_size; ++i) {
-        ret_stack[i] = try_move(ret_stack[i]);
+
+    size_t ret_total = ret_size + (ret_fp - ret_base);
+    for(i = 0; i < ret_total; ++i) {
+        ret_base[i] = try_move(ret_base[i]);
+    }
+
+    for(i = 0; i < upd_size; ++i) {
+        upd_base[i].updatable =
+            try_move(upd_base[i].updatable);
     }
 
 #ifdef GEN_TIMING_INFO
@@ -189,10 +216,18 @@ void gc(void)
 #endif
 }
 
+////////////////////////////////////////////////////////////////////////
+// other generic operations
+////////////////////////////////////////////////////////////////////////
+_Bool need_update(void)
+{
+    return upd_size > 0;
+}
+
 void push(closure *p)
 {
     assert(arg_size < ARG_SIZE_MAX);
-    arg_stack[arg_size++] = p;
+    arg_fp[arg_size++] = p;
 }
 
 void pop(size_t n)
@@ -204,7 +239,36 @@ void pop(size_t n)
 void push_ret(closure *p)
 {
     assert(ret_size < RET_SIZE_MAX);
-    ret_stack[ret_size++] = p;
+    ret_fp[ret_size++] = p;
+}
+
+void push_upd_frame(void)
+{
+    assert(upd_size < UPD_SIZE_MAX);
+
+    upd_base[upd_size].arg_fp = arg_fp;
+    upd_base[upd_size].ret_fp = ret_fp;
+    upd_base[upd_size].arg_size = arg_size;
+    upd_base[upd_size].ret_size = ret_size;
+    upd_base[upd_size].updatable = cur_closure;
+
+    arg_fp += arg_size;
+    ret_fp += ret_size;
+    arg_size = ret_size = 0;
+
+    ++upd_size;
+}
+
+// pop 1 (one) update frame
+void pop_upd_frame(void)
+{
+    assert(upd_size > 0);
+
+    --upd_size;
+    arg_fp = upd_base[upd_size].arg_fp;
+    ret_fp = upd_base[upd_size].ret_fp;
+    arg_size = upd_base[upd_size].arg_size;
+    ret_size = upd_base[upd_size].ret_size;
 }
 
 closure *my_halt(void)
@@ -220,14 +284,14 @@ static closure halt = {my_halt, 0};
 closure *run_next(void)
 {
     if(ret_size != 0) {
-        return ret_stack[--ret_size];
+        return ret_fp[--ret_size];
     } else {
         return &halt;
     }
 }
 
 #define create_ubsym(name,str)                          \
-    static symbol sym_##name = {{run_ubsym, 0}, str};   \
+    static symbol sym_##name = {{run_ubsym, 0}, {str}}; \
     static closure *name = &(sym_##name.c)
 
 create_ubsym(close_paren, ")");
@@ -237,7 +301,7 @@ void move_arg(void)
 {
     size_t i;
     for(i = 0; i < arg_size; ++i) {
-        push_ret(arg_stack[i]);
+        push_ret(arg_fp[i]);
         push_ret(param_sep);
     }
     arg_size = 0;
@@ -248,8 +312,13 @@ void move_arg(void)
 // in the output.
 //
 // How can I solve this problem?
+closure *update_closure(void);
 closure *run_ubsym(void)
 {
+    if(need_update()) {
+        return update_closure();
+    }
+
     char *symbol = (char *)(cur_closure + 1);
 
     if(arg_size > 0) {
@@ -296,12 +365,30 @@ closure *run_apply(void)
     return start[0];
 }
 
+closure *prepare_upd(void)
+{
+    push_upd_frame();
+
+    size_t nfv = cur_closure->fv_cnt;
+    closure **start = (closure **)(cur_closure + 1);
+
+    assert(nfv >= 1);
+
+    size_t i;
+    for(i = nfv - 1; i > 0; --i) {
+        push(start[i]);
+    }
+    return start[0];
+}
+
 /* This is used to get the number of args in a __VA_ARGS__ list. */
 /* Idea from Stefan Reuther, fe5vsq.17c.1@stefan.msgid.phost.de */
 #define PP_NARG(...) (sizeof((closure *[]){__VA_ARGS__})/sizeof(closure *))
 
 #define apply(...)                                                  \
-    make_closure(run_apply, PP_NARG(__VA_ARGS__), __VA_ARGS__)
+    make_closure(prepare_upd, PP_NARG(__VA_ARGS__), __VA_ARGS__)
+/* #define apply(...)                                            \ */
+/*     make_closure(run_apply, PP_NARG(__VA_ARGS__), __VA_ARGS__) */
 
 /* We use an gcc extension here! */
 /* The ## preceeding __VA_ARGS__ will eat the comma right before it if
@@ -309,14 +396,89 @@ closure *run_apply(void)
 #define create_closure(name, ...)                                    \
     make_closure(func_##name, PP_NARG(__VA_ARGS__), ##__VA_ARGS__)
 
-#define need_args(n) assert(arg_size >= n)
-
 closure *get_param(size_t i)
 {
-    return arg_stack[arg_size - 1 - i];
+    return arg_fp[arg_size - 1 - i];
 }
 
 #define def_closure(name) \
     closure *func_##name(void)
 
 //////////////////////////////////////////////////////////////////////
+
+closure *make_upd(closure *ret)
+{
+    // We either allocate a new space or reuse the existing closure.
+    if(ret == NULL) {
+        ret = alloc_heap(arg_size + 1);
+    }
+
+    ret->code = run_apply;
+    ret->fv_cnt = arg_size + 1;
+
+    closure **fv_ptr = (closure **)(ret + 1);
+    fv_ptr[0] = cur_closure;
+
+    size_t i;
+    for(i = 1; i <= arg_size; ++i) {
+        fv_ptr[i] = get_param(i - 1);
+    }
+
+    return ret;
+}
+
+closure *run_indirection(void)
+{
+    closure **fv_ptr = (closure **)(cur_closure + 1);
+    return fv_ptr[0];
+}
+
+// fill a closure with indirection information
+void fill_ind_info(closure *src, closure *dst)
+{
+    assert(src->fv_cnt >= 1);
+    src->code = run_indirection;
+    closure **fv_ptr = (closure **)(src + 1);
+    fv_ptr[0] = dst;
+}
+
+closure *update_closure(void)
+{
+    assert(ret_size == 0);
+    size_t upd_nfv = arg_size + 1;
+
+    closure *old = upd_base[upd_size - 1].updatable;
+
+    closure *upd;
+    if(arg_size == 0) {
+        fill_ind_info(old, cur_closure);
+        upd = cur_closure;
+    } else if(upd_nfv <= old->fv_cnt) {
+        upd = make_upd(old);
+    } else { // indirection
+        upd = make_upd(NULL);
+        fill_ind_info(old, upd);
+    }
+
+    pop_upd_frame();
+    return upd;
+}
+
+//#define need_args(n) assert(arg_size >= n)
+
+// 0. arg_size >= n : nothing happens
+//
+// 1. arg_size < n && upd_size > 0 : perform an update
+//
+// 2. arg_size < n && upd_size == 0 : we need to supply some dummy
+// variable(s) and print out the current function.
+#define need_args(n)                            \
+    do {                                        \
+        if(arg_size < n) {                      \
+            if(need_update()) {                 \
+                return update_closure();        \
+            } else {                            \
+                assert(0);                      \
+            }                                   \
+        }                                       \
+    } while(0)
